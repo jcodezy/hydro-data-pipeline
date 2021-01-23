dda345c3d3313c334b51b9b588cc148ff2dde953
JC <ulatjc@gmail.com>
2021-01-22 17:52:11 -0800

    cleaned up formatting

--- src/selenium_script.py
+++ src/selenium_script.py
@@ -22,7 +22,14 @@ to_date_day = to_date.strftime("%d")
 to_date_month = to_date.strftime("%b")
 to_date_year = to_date.strftime("%Y")
 
-def download_csv_raw(from_date_day=from_date_day, from_date_month=from_date_month, from_date_year=from_date_year, to_date_day=to_date_day, to_date_month=to_date_month, to_date_year=to_date_year):
+def download_csv_raw(
+    from_date_day=from_date_day, 
+    from_date_month=from_date_month, 
+    from_date_year=from_date_year, 
+    to_date_day=to_date_day, 
+    to_date_month=to_date_month, 
+    to_date_year=to_date_year
+):
 
     """
     This function uses Selenium to log into my hydro service provider, navigate to the data export centre 
@@ -45,67 +52,67 @@ def download_csv_raw(from_date_day=from_date_day, from_date_month=from_date_mont
     driver.find_element_by_xpath(""" //div[@id='bchh-loginblock']/button[@id='btnLogin'] """).click()
 
     # Find username & password fields on login page
-    driver.find_element_by_xpath(""" //input[@id='email'] """).send_keys(EMAIL)
+    driver.find_element_by_xpath(" //input[@id='email'] ").send_keys(EMAIL)
     sleep(1)
-    driver.find_element_by_xpath(""" //input[@id='password'] """).send_keys(PASSWORD)
+    driver.find_element_by_xpath(" //input[@id='password'] ").send_keys(PASSWORD)
     sleep(1)
-    driver.find_element_by_xpath(""" //div[@class='formButtons-wrapper']/input[@name='btnSubmit'] """).click()
+    driver.find_element_by_xpath(" //div[@class='formButtons-wrapper']/input[@name='btnSubmit'] ").click()
 
     # hover over 'shortcuts' menu;
     sleep(1)
-    hover_menu = driver.find_element_by_xpath(""" //div[@class='acctsrv_shortcuts'] """)
+    hover_menu = driver.find_element_by_xpath(" //div[@class='acctsrv_shortcuts'] ")
     ActionChains(driver).move_to_element(hover_menu).perform()
 
     # then click 'data export centre'4  
-    driver.find_element_by_xpath(""" //div[@class='extra']/ul/li/a[@class='icon billing_export'] """).click()
+    driver.find_element_by_xpath(" //div[@class='extra']/ul/li/a[@class='icon billing_export'] ").click()
     sleep(1)
-    driver.execute_script("window.scrollTo(0, 500)")
-    driver.find_element_by_id("""data-export-consumption-history""").click()
+    driver.execute_script(" window.scrollTo(0, 500) ")
+    driver.find_element_by_id(" data-export-consumption-history ").click()
     sleep(1) 
-    driver.find_element_by_id("""export-format-csv""").click()
+    driver.find_element_by_id(" export-format-csv ").click()
     sleep(1)
 
     #  Fill out date range box #1 (from date)
-    driver.find_element_by_xpath(""" //input[@id='fromdate-datepicker'] """).click()
+    driver.find_element_by_xpath(" //input[@id='fromdate-datepicker'] ").click()
     sleep(1)
-    select = Select(driver.find_element_by_xpath(""" //select[@class='ui-datepicker-month'] """))
+    select = Select(driver.find_element_by_xpath(" //select[@class='ui-datepicker-month'] "))
     sleep(1)
     select.select_by_visible_text(from_date_month)
     sleep(1)
-    select = Select(driver.find_element_by_xpath(""" //select[@class='ui-datepicker-year'] """))
+    select = Select(driver.find_element_by_xpath(" //select[@class='ui-datepicker-year'] "))
     select.select_by_visible_text(from_date_year)
     sleep(1)
     if from_date_day[0] == '0': 
         from_date_day = from_date_day[1]
-        driver.find_element_by_xpath(f""" //a[@class='ui-state-default' and text()='{from_date_day}'] """).click()
+        driver.find_element_by_xpath(f" //a[@class='ui-state-default' and text()='{from_date_day}'] ").click()
     else:
-        driver.find_element_by_xpath(f""" //a[@class='ui-state-default' and text()='{from_date_day}'] """).click()
+        driver.find_element_by_xpath(f" //a[@class='ui-state-default' and text()='{from_date_day}'] ").click()
     sleep(1)
 
     # Fill out date range box #2 (to date) 
-    driver.find_element_by_xpath(""" //input[@id='todate-datepicker'] """).click()
+    driver.find_element_by_xpath(" //input[@id='todate-datepicker'] ").click()
     sleep(1)
-    select = Select(driver.find_element_by_xpath(""" //select[@class='ui-datepicker-month'] """))
+    select = Select(driver.find_element_by_xpath(" //select[@class='ui-datepicker-month'] "))
     select.select_by_visible_text(to_date_month)
     sleep(1)
-    select = Select(driver.find_element_by_xpath(""" //select[@class='ui-datepicker-year'] """))
+    select = Select(driver.find_element_by_xpath(" //select[@class='ui-datepicker-year'] "))
     select.select_by_visible_text(to_date_year)
     sleep(1)
     if to_date_day[0] == '0':
         to_date_day = to_date_day[1]
-        driver.find_element_by_xpath(f""" //a[@class='ui-state-default' and text()='{to_date_day}'] """).click()
+        driver.find_element_by_xpath(f" //a[@class='ui-state-default' and text()='{to_date_day}'] ").click()
     else:
-        driver.find_element_by_xpath(f""" //a[@class='ui-state-default' and text()='{to_date_day}'] """).click()
+        driver.find_element_by_xpath(f" //a[@class='ui-state-default' and text()='{to_date_day}'] ").click()
     sleep(1)
 
     # Select 'Hourly' interval for file download
-    driver.find_element_by_xpath(""" //div[@class='chosen-container chosen-container-single chosen-container-single-nosearch'] """).click()
+    driver.find_element_by_xpath(" //div[@class='chosen-container chosen-container-single chosen-container-single-nosearch'] ").click()
     sleep(1) 
-    driver.find_element_by_xpath(""" //li[text()='Hourly'] """).click()
+    driver.find_element_by_xpath(" //li[text()='Hourly'] ").click()
     sleep(1) 
 
     # Click export 
-    driver.find_element_by_xpath(""" //input[@id='btnExportData'] """).click()    
+    driver.find_element_by_xpath(" //input[@id='btnExportData'] ").click()    
     sleep(1)
 
     # Make sure file is downloaded before exiting driver
@@ -119,7 +126,7 @@ def download_csv_raw(from_date_day=from_date_day, from_date_month=from_date_mont
                 print("Sucessfully downloaded: ", latest_file)
                 driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME) # Scroll up then click log out before closing down
                 sleep(1)
-                driver.find_element_by_xpath(""" //button[@id='btnLogout'] """).click()
+                driver.find_element_by_xpath(" //button[@id='btnLogout'] ").click()
                 print("Selenium script ran successfully. Exiting . . . .")
                 driver.quit()
                 return f"{DOWNLOAD_PATH}/{latest_file}" 
@@ -129,7 +136,7 @@ def download_csv_raw(from_date_day=from_date_day, from_date_month=from_date_mont
         else:
             timeout+=1 
             sleep(1)
+
     print("Could not download file -- exiting script.")
     driver.quit()
-
     return 
