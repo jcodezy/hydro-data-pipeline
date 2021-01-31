9d0b01172e359a7865e6af03bfa8ac26afc2fb25
JC <ulatjc@gmail.com>
2021-01-31 09:22:53 -0800

    created another pyspark job to run daily analytics

---
+++ pyspark/daily_sum_kwh.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+"""
+Get sum of kwh for each day; this pyspark job to be run on dataproc. 
+"""
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .master('yarn') \
+    .appName('daily-analytics-sum-kwh') \
+    .getOrCreate()
+
+BUCKET = 'hydro-data-spark-bucket'
+spark.conf.set('temporaryGcsBucket', BUCKET)
+
+daily_hydro_history = spark.read.format('bigquery') \
+    .option('table', 'jc-hydro-data:staging.daily_hydro_data') \
+    .load()
+daily_hydro_history.createOrReplaceTempView('daily_hydro_history')
+
+sum_kwh_per_day = spark.sql("""
+SELECT
+    CAST(interval_start_date_time as DATE) as interval_day,
+    SUM(net_consumption_kwh) as sum_kwh
+FROM 
+    daily_hydro_history
+GROUP BY 1
+""")
+sum_kwh_per_day.show()
+sum_kwh_per_day.printSchema()
+sum_kwh_per_day.write.format('bigquery') \
+    .option('table', 'staging.daily_sum_kwh') \
+    .mode('overwrite') \
+    .save()
