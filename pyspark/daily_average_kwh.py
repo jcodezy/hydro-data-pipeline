f32a6d5e65ad58d261b53a277a8971074e25e5ac
JC <ulatjc@gmail.com>
2021-01-31 09:24:21 -0800

    fixed issues that were causing issues on dataproc

--- pyspark/daily_average_kwh.py
+++ pyspark/daily_average_kwh.py
@@ -2,25 +2,21 @@
 """
 To be run in the pyspark_processing DAG on google dataproc.
 """
-import os
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
     .master('yarn') \
-    .appName('bigquery-daily-avg-kwh') \
+    .appName('daily-analytics-avg-kwh') \
     .getOrCreate() 
 
-HYDRO_DATA_PROJECT_ID = os.getenv('HYDRO_DATA_PROJECT_ID')
-STAGING_DATASET = os.getenv('HYDRO_STAGING_DATASET')
-BUCKET = os.getenv('SPARK_BUCKET')
+BUCKET = 'hydro-data-spark-bucket'
 spark.conf.set('temporaryGcsBucket', BUCKET)
 
 # Create a temporary view from existing bq table
 daily_hydro_history = spark.read.format('bigquery') \
-    .option('table', f'{jHYDRO_DATA_PROJECT_ID}.{STAGING_DATASET}.daily_hydro_data') \
+    .option('table', 'jc-hydro-data:staging.daily_hydro_data') \
     .load()
 daily_hydro_history.createOrReplaceTempView('daily_hydro_history')
 
@@ -29,7 +25,8 @@ avg_kwh_per_day = spark.sql("""
 SELECT 
     CAST(interval_start_date_time as DATE) as interval_day, 
     AVG(net_consumption_kwh) as avg_kwh 
-FROM daily_hydro_history 
+FROM 
+    daily_hydro_history 
 GROUP BY 1
 """)
 avg_kwh_per_day.show()
